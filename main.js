2a5d5faecede3e1f9da91602796990635752c7db
Kevin Bandison <kbandison@gmail.com>
2022-10-11 15:19:46 -0400

    completed assignment

--- main.js
+++ main.js
@@ -2,6 +2,43 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function isAdmin(user) {
+    if (user.userRole === 'ADMIN') {
+        return true;
+    } else{
+        return false;
+    }
+}
+
+function getEmail(user){
+    let email = `${user.firstName}.${user.lastName}@codeimmersives.com`;
+    return email.toLowerCase();
+}
+
+function getPlaylistLength(playlist) {
+    return playlist.songs.length;
+}
+
+function getHardestHomework(homeworkArr){
+    if(homeworkArr.length === 0){
+        return '';
+    }
+    let lowestScore = homeworkArr[0];
+    for(let i = 0; i < homeworkArr.length; i++){
+        if (homeworkArr[i].averageScore < lowestScore.averageScore){
+            lowestScore = homeworkArr[i];
+        }
+    }
+    return lowestScore.name;
+}
+
+function createPhonebook(namesArr, numbersArr){
+    let phonebook = {};
+    for (let i = 0; i < namesArr.length; i++){
+        phonebook[namesArr[i]] = numbersArr[i];
+    }
+    return phonebook;
+}
 
 
 // ┌─────────────────────────────────────┐
